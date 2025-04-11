681864a8c6196071054723ae6b27a1eaf2824ad5
Mahib09 <magarmahib@gmail.com>
2025-04-11 00:15:36 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div className="bg-background text-4xl flex items-center justify-center">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Game = () => {
+  return (
+    <div className="bg-gameBackground max-w-[1000px] w-full h-[100vh]">
+      Game
+    </div>
+  );
+};
+
+export default Game;
